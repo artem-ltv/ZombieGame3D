5eee0aa59d3542695c2c24a42188ab660d347159
artem-ltv <litwinenkoartem1@gmail.com>
2022-04-20 16:51:09 +0300

    Add NavMesh

--- Assets/Scripts/ZombieDamage.cs
+++ Assets/Scripts/ZombieDamage.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class ZombieDamage : MonoBehaviour
 {
     [SerializeField] private int _damage;
+    [SerializeField] private Image _damageBackgrond;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out Player player))
         {
             player.ApplyDamage(_damage);
+            _damageBackgrond.DOFade(0.5f, 0.2f);
+            _damageBackgrond.DOFade(0f, 0.7f).SetDelay(0.2f);
         }
     }
 }
