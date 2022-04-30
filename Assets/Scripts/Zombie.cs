8a433cff13086284ec42df19f5373a8de1392e82
artem-ltv <60981162+artem-ltv@users.noreply.github.com>
2022-04-30 13:01:40 +0300

    Modification

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(CapsuleCollider))]
 public class Zombie : Player
 {
     public UnityAction ZombieDie;
+
+    private CapsuleCollider _collider;
+
+    private void Start()
+    {
+        _collider = GetComponent<CapsuleCollider>();    
+    }
 
     private void Update()
     {
@@ -13,6 +21,7 @@ public class Zombie : Player
 
     protected override void Die()
     {
+        _collider.enabled = false;
         ZombieDie?.Invoke();
     }
 }
