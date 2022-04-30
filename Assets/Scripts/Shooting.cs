d7b6029aa3223e01a841fec3c9c8942fbc013f8d
artem-ltv <60981162+artem-ltv@users.noreply.github.com>
2022-04-30 14:13:58 +0300

    Add Gun

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -4,11 +4,13 @@ public class Shooting : MonoBehaviour
 {
     [SerializeField] private float _shotRange;
     [SerializeField] private Camera _camera;
+    [SerializeField] private Animation _shootAnimation;
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            _shootAnimation.Play();
             RaycastHit hit;
 
             if (Physics.Raycast(_camera.transform.position, _camera.transform.forward, out hit, _shotRange))
