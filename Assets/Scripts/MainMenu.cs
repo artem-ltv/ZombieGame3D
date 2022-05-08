3b57b628937d22cb254683bee9e79332dd20100d
artem-ltv <60981162+artem-ltv@users.noreply.github.com>
2022-05-08 13:23:04 +0300

    Add menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button _buttonPlay;
+    [SerializeField] private Button _buttonOptions;
+    [SerializeField] private Button _buttonQuit;
+
+    private void Start()
+    {
+        _buttonPlay.onClick.AddListener(Play);
+        _buttonOptions.onClick.AddListener(ShowOptions);
+        _buttonQuit.onClick.AddListener(Quit);
+    }
+
+    private void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void ShowOptions()
+    {
+        Debug.Log("Options");
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+}
